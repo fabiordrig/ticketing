9f69e5cab159d66ce38632991f13ac2cc3662352
Fabio Rodrigues <fabiordrig@gmail.com>
2020-09-09 19:03:14 -0300

    feat(middleware): adding new handler error

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,13 +1,26 @@
 import { Request, Response, NextFunction } from "express";
+import { RequestValidationError } from "../errors/request-validation-error";
+import { DatabaseConnectionError } from "../errors/database-conn-error";
+
 export const errorHandler = async (
   err: Error,
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  console.log("Deu ruim", err);
+  if (err instanceof RequestValidationError) {
+    const errors = err.errors.map((error) => {
+      return { message: error.msg, field: error.param };
+    });
 
-  res.status(400).send({
-    message: "Deu ruim",
+    return res.status(400).send({ errors });
+  }
+
+  if (err instanceof DatabaseConnectionError) {
+    return res.status(500).send({ errors: [{ message: err.reason }] });
+  }
+
+  res.status(500).send({
+    errors: [{ message: "Unreachable" }],
   });
 };
