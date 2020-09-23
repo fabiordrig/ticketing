ef258cb3cef3103e96835b851dbb282b5bd361b8
Fabio Rodrigues <fabiordrig@gmail.com>
2020-09-23 18:54:09 -0300

    feat(middleware): adding new route handler

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,20 +1,11 @@
-import express, { Request, Response } from "express";
-import { body } from "express-validator";
-import jwt from "jsonwebtoken";
+import express from "express";
+
+import { currentUser } from "../middlewares/current-user";
 
 const router = express.Router();
 
-router.get("/api/users/currentUser", (req, res, next) => {
-  if (!req.session?.jwt) {
-    return res.send({ currentUser: null });
-  }
-
-  try {
-    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
-    res.send({ currentUser: payload });
-  } catch (error) {
-    res.send({ currentUser: null });
-  }
+router.get("/api/users/currentUser", currentUser, (req, res) => {
+  res.send({ currentUser: req.currentUser || null });
 });
 
 export { router as currentUserRouter };
