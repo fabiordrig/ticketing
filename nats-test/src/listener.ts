579ad8c51ef6747156b069c0e873b5714eac6710
Fabio Rodrigues <fabiordrig@gmail.com>
2020-10-07 21:14:57 -0300

    feat(nats): adding queue group on listener service

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,17 +1,24 @@
-import nats from "node-nats-streaming";
+import nats, { Message } from "node-nats-streaming";
+import { randomBytes } from "crypto";
+
+const queueGroup = "orders-service-queue-group";
 
 console.clear();
 
-const stan = nats.connect("ticketing", "123", {
+const stan = nats.connect("ticketing", randomBytes(4).toString("hex"), {
   url: "http://localhost:4222",
 });
 
 stan.on("connect", () => {
   console.log("Listener connected to NATS");
 
-  const subscription = stan.subscribe("ticket:created");
+  const subscription = stan.subscribe("ticket:created", queueGroup);
 
-  subscription.on("message", (msg) => {
-    console.log("message received");
+  subscription.on("message", (msg: Message) => {
+    const data = msg.getData();
+
+    if (typeof data === "string") {
+      console.log(`Received event #${msg.getSequence()}, with data ${data}`);
+    }
   });
 });
