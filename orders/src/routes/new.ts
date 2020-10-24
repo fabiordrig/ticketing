6eb30db872bb4ff4debe0d3cbf2b622b7de55b6f
Fabio Rodrigues <fabiordrig@gmail.com>
2020-10-24 13:19:57 -0300

    feat(orders): adding creation orders route

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,9 +1,27 @@
+import mongoose from "mongoose";
 import express, { Request, Response } from "express";
+import { body } from "express-validator";
+import {
+  requireAuth,
+  validateRequest,
+  HTTP_STATUS_CODE,
+} from "@commons-ticketing/commons";
 
 const router = express.Router();
 
-router.post("/api/orders", async (req: Request, res: Response) => {
-  res.send({});
-});
+router.post(
+  "/api/orders",
+  requireAuth,
+  [
+    body("ticketId")
+      .notEmpty()
+      .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
+      .withMessage("TicketID must be provided"),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    res.send({});
+  }
+);
 
 export { router as newOrderRouter };
