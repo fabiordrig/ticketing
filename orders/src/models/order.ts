5874886d09561d2a80014899af94735da0fe418d
Fabio Rodrigues <fabiordrig@gmail.com>
2020-10-26 18:52:15 -0300

    feat(orders): adding order and ticket model

---
+++ orders/src/models/order.ts
@@ -0,0 +1,71 @@
+import mongoose from "mongoose";
+import { OrderStatus } from "@commons-ticketing/commons";
+import { TicketDoc } from "./ticket";
+
+export { OrderStatus };
+
+// An interface that describes the properties
+// that are required to create a new Order
+interface OrderAttrs {
+  userId: string;
+  status: OrderStatus;
+  expiresAt: Date;
+  ticket: TicketDoc;
+}
+
+// An interface that describes the properties
+// that a Order Model has
+interface OrderModel extends mongoose.Model<OrderDoc> {
+  build(attrs: OrderAttrs): OrderDoc;
+}
+
+// An interface that describes the properties
+// that a Order Document has
+interface OrderDoc extends mongoose.Document {
+  userId: string;
+  status: OrderStatus;
+  expiresAt: Date;
+  ticket: TicketDoc;
+}
+
+const orderSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+      enum: Object.values(OrderStatus),
+      default: OrderStatus.CREATED,
+    },
+    expiresAt: {
+      type: mongoose.Schema.Types.Date,
+    },
+    ticket: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Ticket",
+    },
+  },
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+
+        delete ret._id;
+        delete ret.__v;
+      },
+    },
+  }
+);
+
+orderSchema.pre("save", async function (done) {});
+
+orderSchema.statics.build = (attrs: OrderAttrs) => {
+  return new Order(attrs);
+};
+
+const Order = mongoose.model<OrderDoc, OrderModel>("Order", orderSchema);
+
+export { Order };
--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -5,7 +5,11 @@ import {
   requireAuth,
   validateRequest,
   HTTP_STATUS_CODE,
+  OrderStatus,
+  NotFoundError,
+  UnprocessableEntity,
 } from "@commons-ticketing/commons";
+import { Ticket, Order } from "../models";
 
 const router = express.Router();
 
@@ -20,6 +24,27 @@ router.post(
   ],
   validateRequest,
   async (req: Request, res: Response) => {
+    const { ticketId } = req.body;
+    // Find the ticket the user is trying to order in the database
+    const ticket = await Ticket.findById(ticketId);
+
+    if (!ticket) {
+      throw new NotFoundError();
+    }
+
+    // Make sure that this ticket is already not reserved
+    const isReserved = await ticket.isReserved();
+
+    if (isReserved) {
+      throw new UnprocessableEntity("Ticket already reserved");
+    }
+
+    // Calculate expiration date for this order
+
+    // Build the order and persist in the database
+
+    // Publish an event saying that an order was created
+
     res.send({});
   }
 );
